164f05b8d322e06189e5c87c02eeed4100b51bc8
Gelande-1234 <angegelande@gmail.com>
2025-05-15 11:41:57 +0200

    configuration de prometheus et grafana

--- server/core/urls.py
+++ server/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import test_api
+from .metrics import metrics_view  # <- ajouter cette ligne
 
 urlpatterns = [
     path('test/', test_api),
+    path('metrics/', metrics_view),  # <- ajouter ce endpoint
 ]
+
 
